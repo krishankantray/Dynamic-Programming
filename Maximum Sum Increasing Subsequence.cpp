a32398e9c219714a801fa7752ff908766205361c
kkdp <35932254+kkdp@users.noreply.github.com>
2018-03-19 01:50:41 +0530

    Create Maximum Sum Increasing Subsequence.cpp

---
+++ Maximum Sum Increasing Subsequence.cpp
@@ -0,0 +1,38 @@
+        #include <iostream>
+        #include <cstdio>
+        #include <vector>
+        #include <set>
+        #include <climits>
+        #include <map>
+        #include <algorithm>
+        #include <cmath>
+        #include <utility>
+        #include <string>
+        #include <cstring>
+        #include <deque>
+        using namespace std;
+        #define ll long long int
+        #define pb push_back
+        #define mp make_pair
+        #define ull unsigned long long int
+        
+        int main()
+        {
+            int n;
+            cin>>n;
+            int arr[n];
+            for(int i=0; i<n; ++i) cin>>arr[i];
+            vector<int>dp(n,1);
+
+            for(int i=1; i<n ;++i)
+            {
+                for(int j=0; j<i; ++j)
+                {
+                    if(arr[i]>arr[j])
+                        dp[i]=max(dp[i],dp[j]+arr[i]);
+                }
+            }
+            cout<<*max_element(dp.begin(), dp.end());
+
+            return 0;
+        }
